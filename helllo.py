e6f3440e147e0f55180182966faa3755666dade0
Jijhoon <darkmine789@naver.com>
2024-01-15 16:59:57 +0900

    assignment of while

--- helllo.py
+++ helllo.py
@@ -1,10 +1,19 @@
-letter = input('Input alphabet letter : ')
-# vowels = {'a', 'e', 'i', 'o','u'} // set. set은 바꿀수 없는 가변상태로 되어있다.
-vowels = "aeiou" # str 이경우에는 중간 문자를 바꿀 수 있다.
-print(type(vowels))
-if letter in vowels : # in이 함수가 되는 것이다.
-    print(f'{letter} is a vowel!')
-else :
-    print(f'{letter} is a consonant!')
+#(100°F-32) * 5/9 = 37.778°C 화씨를 섭씨로 바꾸기
+
+while True :
+    menu = input("\n\n1)Fahrenheit -> Celsius \n2)Celsius -> Fahrenheit \n3)Quit\n write what you want : ")
+    if menu == '1' :
+        Fahrenheit = float(input('Input Fahrenheit : '))
+        print(f'\nFahrenheit : {Fahrenheit}F, Celsius : {((Fahrenheit-32.0) * 5/9):.4f}C')
+        print(menu)
+    elif menu == '2' :
+        Celsius = float(input('Input Celsius : '))
+        print(f'\nCelsius : {Celsius}C, Fahrenheit : {((Celsius*9/5) +32.0):.4f}F')
+        print(menu)
+    elif menu == '3':
+        print("Good Bye! See you again"); break
+    else :
+        print('You write the wrong number. Try again!')
+
 
 
--- hello file.py
+++ hello file.py
@@ -17,14 +17,18 @@
    #왜 인지 작동이 안된다..
 
 
-base_number = int(input("밑을 입력하시오 :"))
-exponent_number = int(input("지수를 입력하시오 :"))
-print(type(base_number), type(exponent_number))
+#base_number = int(input("밑을 입력하시오 :"))
+#exponent_number = int(input("지수를 입력하시오 :"))
+#print(type(base_number), type(exponent_number))
 # f -string
-print(f'밑은{base_number}, 지수는 {exponent_number}, 결과 값은 {pow(base_number ** exponent_number)}')
+#print(f'밑은{base_number}, 지수는 {exponent_number}, 결과 값은 {pow(base_number ** exponent_number)}')
 # format function
 # rint('밑은 {0}, 지수는 {1}, 결과 값은 {2}')
 
 # C언어에서는
-print('밑은 %d, 지수는 %d, 결과 값은 %d' % (base_number, exponent_number, pow(base_number, exponent_number)))
+#print('밑은 %d, 지수는 %d, 결과 값은 %d' % (base_number, exponent_number, pow(base_number, exponent_number)))
 
+
+    l = [1,3,3,2,4,4] #list
+    s = {1,3,3,2,4,4} #set
+        print(l,s) # list : [1,3,3,2,4,4], set : {1,2,3,4}
