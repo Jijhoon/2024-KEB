42369dafa49f3c334f6d23c1f483865608528795
Jijhoon <darkmine789@naver.com>
2024-01-15 14:15:56 +0900

    about ASCII code

--- helllo.py
+++ helllo.py
@@ -1,16 +1,8 @@
-# first_number = int(input("First number : "))
-# second_number = int(input("Second number : "))
-# quotient = first_number // second_number #몫
-# remainder = first_number % second_number #나머지
-# print(f'몫은 {quotient} 나머지는 {remainder}입니다.')
-
-# 같은 코드를 쉽게 짜보자
-
-first_number = int(input('First number : '))
-second_number = int(input("Second number : "))
-print(f'몫은 {divmod(first_number, second_number)[0]}, 나머지는 {divmod(first_number, second_number)[1]}입니다.')
-
-first_number = int(input('First number : '))
-second_number = int(input("Second number : "))
-result = divmod(first_number, second_number)
-print(f'몫은 {result[0]}, 나머지는 {result[1]}입니다.')
+dec = 65 #10 진수 (decimal)
+octal = 0o101 #8 진수
+hexadeciaml = 0x41 #16 진수
+binary = 0b01000001 #2 진수
+print(dec, octal, hexadeciaml, binary)
+print(chr(binary)) #아스키코드로 보여주는 함수이다.
+print(chr(dec), chr(octal), chr(hexadeciaml), chr(binary))
+print(ord('B'), ord('Z'), ord('a'), ord('2')) #66, 90, 97, 50 아라비아 숫자는 48부터 시작한다 -> 0 = 48 / a 는 97 / A 는 65
