c6f80f63ba7454bd18616940cb93bf5b85c07d27
Jijhoon <darkmine789@naver.com>
2024-01-15 14:43:39 +0900

    Fahrenheit & Celsius

--- helllo.py
+++ helllo.py
@@ -1,8 +1,15 @@
-dec = 65 #10 진수 (decimal)
-octal = 0o101 #8 진수
-hexadeciaml = 0x41 #16 진수
-binary = 0b01000001 #2 진수
-print(dec, octal, hexadeciaml, binary)
-print(chr(binary)) #아스키코드로 보여주는 함수이다.
-print(chr(dec), chr(octal), chr(hexadeciaml), chr(binary))
-print(ord('B'), ord('Z'), ord('a'), ord('2')) #66, 90, 97, 50 아라비아 숫자는 48부터 시작한다 -> 0 = 48 / a 는 97 / A 는 65
+#(100°F-32) * 5/9 = 37.778°C 화씨를 섭씨로 바꾸기
+Fahrenheit = float(input('Input Fahrenheit : '))
+print(f'Fahrenheit : {Fahrenheit}F, Celsius : {((Fahrenheit-32.0) * 5/9):.4f}C')
+
+#문자열 타입으로 전환!
+print(str(98.6))
+print(str(1.0e4))
+
+Hi= 'a, b, c, d' # Hi는 문자열의 공백을 포함한다.
+print(Hi[1]), print(Hi[0]) #a=0 , 쉼표 공백 모두 자리를 차지하는 것이다. 이렇게 작성하면 줄바꿔서 작성됨
+print(Hi[1],Hi[0]) # 이렇게 작성하면 한 줄에 작성되는데 여기에 띄어쓰기인 \n을 작성하면 오류가 발생함.
+#\n 은 줄바꿈을 의미한다.
+#For example
+palindrome = 'A man,\nA plan,\nA canal: Panama.'
+print(palindrome)
