5d5271973d73afaf21f2d46bf9de3bb1bccedd23
Jijhoon <darkmine789@naver.com>
2024-01-15 13:44:11 +0900

    about divmod

--- helllo.py
+++ helllo.py
@@ -1,1 +1,16 @@
-print("hello python!")
+# first_number = int(input("First number : "))
+# second_number = int(input("Second number : "))
+# quotient = first_number // second_number #몫
+# remainder = first_number % second_number #나머지
+# print(f'몫은 {quotient} 나머지는 {remainder}입니다.')
+
+# 같은 코드를 쉽게 짜보자
+
+first_number = int(input('First number : '))
+second_number = int(input("Second number : "))
+print(f'몫은 {divmod(first_number, second_number)[0]}, 나머지는 {divmod(first_number, second_number)[1]}입니다.')
+
+first_number = int(input('First number : '))
+second_number = int(input("Second number : "))
+result = divmod(first_number, second_number)
+print(f'몫은 {result[0]}, 나머지는 {result[1]}입니다.')
--- hello file.py
+++ hello file.py
@@ -1,14 +1,13 @@
 
 #"univ" = "inha university" // 불가능! literal : 소스코드의 고정된 값
 
-num = input("숫자 10을 입력해보시오 : ")
-if int(num) < 10:
-    print("too low")
-elif int(num) > 10:
-    print("too high")
-else :
-    print("correct")
-
+# num = input("숫자 10을 입력해보시오 : ")
+# if int(num) < 10:
+#     print("too low")
+# elif int(num) > 10:
+#     print("too high")
+# else :
+#     print("correct") // 잘 작동한다.
 
     #같은 내용으로 바다코끼리 연산자를 사용가능하다
    # if (num := int(input("숫자 10을 입력해보삼 : "))) < 10:
@@ -16,3 +15,16 @@ else :
    # else (num := int(input("숫자 10을 입력해보삼 : "))) > 10:
    #     print("10보다 큰 숫자임")
    #왜 인지 작동이 안된다..
+
+
+base_number = int(input("밑을 입력하시오 :"))
+exponent_number = int(input("지수를 입력하시오 :"))
+print(type(base_number), type(exponent_number))
+# f -string
+print(f'밑은{base_number}, 지수는 {exponent_number}, 결과 값은 {pow(base_number ** exponent_number)}')
+# format function
+# rint('밑은 {0}, 지수는 {1}, 결과 값은 {2}')
+
+# C언어에서는
+print('밑은 %d, 지수는 %d, 결과 값은 %d' % (base_number, exponent_number, pow(base_number, exponent_number)))
+
