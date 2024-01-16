a2ec305dd304004652a6cf777aa4c1cfc6d46591
Jijhoon <darkmine789@naver.com>
2024-01-16 14:44:12 +0900

    prime number v.2

--- helllo.py
+++ helllo.py
@@ -1,29 +1,18 @@
-#prime number
+# prime number
 number = int(input("Input number : "))
-cnt = 0
-i = 1
-while i <= number:
-    if number % i == 0:
-        cnt = cnt + 1
-    i = i + 1
-
-if cnt == 2:
-    print(f'{number} is prime number')
-else:
+is_prime = True # integer type -> bool
+if number < 2:
     print(f'{number} is not prime number')
-
-    # prime number
-    number = int(input("Input number : "))
-    cnt = 0
+else:
     i = 2
     while i < number:
         if number % i == 0:
-            cnt = cnt + 1
+            is_prime = False # remove +
             break
-        print(i, end=' ') # 계산하는 과정을 보여줌.
+        #print(i, end=' ') # 계산하는 과정을 보여줌.
         i = i + 1
 
-    if cnt == 0:
+    if is_prime: # remove ==
         print(f'{number} is prime number')
     else:
         print(f'{number} is not prime number')
