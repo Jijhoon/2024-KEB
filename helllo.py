6d9ef4b997cd7ff05724c04857318a23b210114a
Jijhoon <darkmine789@naver.com>
2024-01-16 15:02:26 +0900

    prime number with for

--- helllo.py
+++ helllo.py
@@ -4,13 +4,11 @@ is_prime = True # integer type -> bool
 if number < 2:
     print(f'{number} is not prime number')
 else:
-    i = 2
-    while i < number:
+    for i in range(2, number):
         if number % i == 0:
             is_prime = False # remove +
             break
         #print(i, end=' ') # 계산하는 과정을 보여줌.
-        i = i + 1
 
     if is_prime: # remove ==
         print(f'{number} is prime number')
