71af3bb3625d69d4ff11286101dba12fbd094bfd
Jijhoon <darkmine789@naver.com>
2024-01-16 13:37:20 +0900

    isalnum() & capitalize

--- helllo.py
+++ helllo.py
@@ -1,16 +1,20 @@
-subject = "python c++ database linux"
-while True :
-    word = input("press the word : ")
-    if subject.find(word) == -1:
-        print("다시 입력하시오")
-    else :
-        print(subject.find(word));
-        break
-# index는 값을 찾지 못하면 결과 값이 Error이다. vs find는 값을 찾지 못하면 결과 값이 -1이다.
-##preview
-#try:
-#    print(~~)
-#except ValueError :
-#count 함수 : 몇 개 있는 지 알려준다.
+#isalnum() : 문자열이 알파벳([a-zA-Z])과 숫자([0-9])로만 구성되었는지 확인하는 파이썬 문자열 메소드
+subject = "pythoncadataba!!!!se123linux"
+print(subject.isalnum()) #False 느낌표가 존재하기 때문에 False가 return되었다.
+subject = "python cad ataba e123linux"
+print(subject.isalnum()) #False 공백이 존재하기 때문에 False가 return되었다.
+subject = "pythoncadatabae123linux"
+print(subject.isalnum()) #True 문자열이 알파벳과 숫자로만 이루어졌기 때문이 True가 return되었다.
 
-#is 는 True 또는 False를 return한다.
+setup = "....sdafadsf....dsadsafsd ...."
+print(setup.strip(".")) # 앞 뒤로 붙은 ... 만 없애기 때문에 중간 부분의 ...이 없어지지 않음.
+
+#capitalize() : 대문자로 바꾼다.
+print(setup.capitalize())
+print(setup.title())
+#정렬. 배치
+print(setup)
+# %s : string(문자열) / %x : 16진수 / %o : 8진수 / %d : decimal(십진수) / %f : float(실수형) / %e : 지수형  / %g : 실수형 자동출력
+# cf) int : 정수형 integer
+
+
