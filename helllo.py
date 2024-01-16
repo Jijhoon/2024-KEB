ff8aff230152e104df1ece6f3839bee3f5b39de7
Jijhoon <darkmine789@naver.com>
2024-01-16 13:07:29 +0900

    ifind with while

--- helllo.py
+++ helllo.py
@@ -1,26 +1,16 @@
-#replace 대체하기.
-course = "2024 KEB bootcamp"
-print(course)
-print(course.replace('KEB', 'Inha'))
-# 2024 Inha bootcamp // 여기서는 잠깐 변화된거다. course = course.replace()를 지정해줘야 완전 변화됨.
-print(course) #2024 KEB bootcamp 다시 나타남
+subject = "python c++ database linux"
+while True :
+    word = input("press the word : ")
+    if subject.find(word) == -1:
+        print("다시 입력하시오")
+    else :
+        print(subject.find(word));
+        break
+# index는 값을 찾지 못하면 결과 값이 Error이다. vs find는 값을 찾지 못하면 결과 값이 -1이다.
+##preview
+#try:
+#    print(~~)
+#except ValueError :
+#count 함수 : 몇 개 있는 지 알려준다.
 
-course = course.replace('KEB', 'Inha', 10) # 2024 Inha bootcamp // count는 up to 개념 ~개까지 교환
-print(course)
-#strip 양쪽의 공간제거. lstip 왼쪽의 공간제거. rstrip 오른쪽의 공간제거
-world = "      earth        "
-print(world)
-print(world.strip())
-new_world = "!!!!earth!!!!"
-print(new_world)
-print(new_world.strip('!'))
-nice_world = "!@#$****@@!!#earth#!@#@!@#$@@***" #다른 언어가 한개라도 껴 있으면 제대로 안 지워진다. 예를들어 중간에 %, ^등이 껴잇으면 달라짐.
-print(nice_world)
-print(nice_world.strip("!#$@*"))
-
-#순서찾는 함수 find, index // find는 찾지 못했을 때 -1을 출력 -> if문을 통해서 print 가능.
-# index는 ValueError : substring not found        find와 index는 결과를 찾지 못했을 때 달라진다.
-foem = "hello nice to meetddd sada you the coke fsdaasdasdasdasmellon here you are."
-word = "the"
-print(foem.find(word))# the는 앞에서부터 23번째이다.
-print(foem.rfind(word))
+#is 는 True 또는 False를 return한다.
