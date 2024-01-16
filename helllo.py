4992edf8921f7aa3fc5785caffedabd17afb460f
Jijhoon <darkmine789@naver.com>
2024-01-16 11:14:17 +0900

    about split & join

--- helllo.py
+++ helllo.py
@@ -1,16 +1,15 @@
-#university = r"Inha\nuniversity" # r(로스팅)을 문자열 앞에 붙이면 \n이 줄바꿈을 하지 않고 그대로 나오게 된다.
-#print(university)
+#Split 문자열을 분리해서 리스트에 담는다 (매우 중요)
+course = "2024 KEB bootcamp"
+#print(course)
+#list_course = course.split()   #['2024', 'KEB', 'bootcamp'] 띄어쓰기를 한 것을 기준으로 3개로 분리된다.
+#list_course = course.split('B')   #['2024 KE', ' bootcamp'] B를 기준으로 나뉜다.
+#print(list_course)
 
-#number1 = str(input("first number :")) # str이 없다면 그냥 숫자끼리 더해짐.
-#number2 = input("second number : ") # str() -> 문자열로 바꾸는 함수.
-#print(number1, number2) # concatenation
-#print(number1 * 3) #duplication
-#print(number1 + 3) #type error -> 문자열 + 숫자.
-#문자열을 추출할 때 교체를 하고 싶다!
-name = "Henny"
-name.replace( "H",  "P") # 이거는 안 바뀌는데?
+numbers = input("first_number second_number : ").split() #numbers : ["first_number", "second_number"]
+#print(numbers[0] + numbers[1]) #concatenation 문자열이 더해진 상태가 된다.
+print(int(numbers[0]) + int(numbers[1])) #arithmetic operation    [첫번째 방 + 두번째 방]
 
-print(name)
-print("P" + name[1::1]) # [이상:미만:차례]!@!!!@!@!@!  이상(start) 미만(end) // 차례(step)는 0부터 X 1부터 시작한다.
-
-
+#Join 문자열 -> 리스트로 변환한다.
+subjects = ["pyrhon", 'c++', "database"]
+subjects_string = ' / '.join(subjects) #(공백)/(공백)을 사이에 끼워넣은 상태로 list안의 방들이 합쳐진다.
+print("합쳐진 모습 : " + subjects_string) #합쳐진 모습 : pyrhon / c++ / database
