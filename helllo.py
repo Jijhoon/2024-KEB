43a8729426d57b40795177e44279c4866e57e5ca
Jijhoon <darkmine789@naver.com>
2024-01-16 14:33:32 +0900

    prime number

--- helllo.py
+++ helllo.py
@@ -1,8 +1,29 @@
-#rjust(20) 오른쪽으로 20만큼 정렬 / ljust(20) 왼쪽으로 20만큼 정렬
-subject = {
-    'python': 'kim',
-    'c++': 'sung',
-    'data structure': ['kim2', 'kim3'],
-    'database': 'kang'
-    }
-print("{0[python]} {0[data structure]}".format(subject))
+#prime number
+number = int(input("Input number : "))
+cnt = 0
+i = 1
+while i <= number:
+    if number % i == 0:
+        cnt = cnt + 1
+    i = i + 1
+
+if cnt == 2:
+    print(f'{number} is prime number')
+else:
+    print(f'{number} is not prime number')
+
+    # prime number
+    number = int(input("Input number : "))
+    cnt = 0
+    i = 2
+    while i < number:
+        if number % i == 0:
+            cnt = cnt + 1
+            break
+        print(i, end=' ') # 계산하는 과정을 보여줌.
+        i = i + 1
+
+    if cnt == 0:
+        print(f'{number} is prime number')
+    else:
+        print(f'{number} is not prime number')
