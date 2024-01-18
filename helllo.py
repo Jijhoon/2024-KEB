e2a4f9a1a7b888658641e50500d70aeda9b8e79c
Jijhoon <darkmine789@naver.com>
2024-01-18 19:00:28 +0900

    day 5 assignment & review

--- helllo.py
+++ helllo.py
@@ -1,61 +1,66 @@
-#lambda : 영구적인 함수 // 자주쓰는 함수라서 미리 만들어 놓는다.
-def squares(n):
-    return n * n
-even_numbers = [i for i in range(51) if i % 2 == 0]
-print(even_numbers)
-print(tuple(map(squares, even_numbers)))
-print(tuple(map(lambda x: x**2, even_numbers)))
+#9.1 Define a function called good() that returns the following list: ['Harry', 'Ron', 'Hermione'].
+def good():
+    return ['Harry', 'Ron', 'Hermione']
+print(good())
 
-# 구조 >>> lambda input: input이 실행되는 식.
-
-# generator는 메모리에 저장하지 않는다. 숫자 생성하고 다음숫자 생성할때 이전 숫자를 지워버린다. ex) range()
-
-def my_range(first = 0, last = 5, step = 1):
-    number = first
-    while number < last:
-        yield number #함수의 실행 상태를 유지하면서 값을 순차적으로 반환 // 큰 데이터를 처리할 때 메모리를 효율적으로 사용
-        number += step
-r = my_range()
-print(r,type(r))
-
-for x in r : # 0~4까지 출력됨.
-    print(x)
-for x in r: #한번 사용한 값들이기 때문에 휘발해버렸음.
-    print(x)
-
-#Decorators  OCP 개방폐쇄원칙(open closed principle) : 확장에는 열려있고 수정에는 닫혀야한다.
-#document_it() // 함수 출력과 인수 출력을 해준다.
-
-# decorator
-def description(f):  # closure
-    def inner(*args):
-        print(f.__name__)
-        print(f.__doc__)
-        r = f(*args)
-        return r
-
-    return inner
+# 9.2 Define a generator function called get_odds() that returns the odd numbers from range(10).
+# Use a for loop to find and print the third value returned.
+def get_odds():
+    for i in range(1, 10):
+        if i % 2 == 1:
+            yield i
 
 
-def squares(n):
-    """
-    제곱 함수
-    """
-    return n * n
+for number in get_odds():
+    print(number)
 
-@description # 얘가 붙음으로써 데코레이팅이 된다.
-def power(b, e):
-    """
-    거듭제곱 함수
-    """
-    result = 1
-    for _ in range(e):
-        result = result * b
-    return result
+#9.3 Define a decorator called test that prints 'start' when a function is called, and 'end' when it finishes.
+
+def test(f):
+    def wrapper(*args, **kwargs):
+        print("start")
+        result = f(*args, **kwargs)
+        print("end")
+        return result
+    return wrapper
+
+@test
+def func():
+    return print("hi")
+func()
 
 
-f1 = description(squares)
-print(f1(9))
-print(power(2, 10))
-# f2 = description(power)
-# print(f2(2, 10))
+def pupupu(arg,result = []):
+    result.append(arg)
+    return print(result)
+
+pupupu("a")
+pupupu("b")
+
+def popopo(arg, result = None):
+    if result is None:
+        result = []
+        result.append(arg)
+    return print(result)
+
+popopo("a")
+popopo("b")
+
+def ricecake(saysomething):
+        def cake():
+            return "I am delicious'%s'" % saysomething
+        return cake
+
+a = ricecake("hi") #이런 상태를 만들어야만 내부의 함수를 볼 수 있다. -> I am delicious'hi'
+print(a())
+print(ricecake("hi")) # >>> <function ricecake.<locals>.cake at 0x0000021B1EE0F420>
+
+def newspaper(word):
+    return print(word.capitalize() + "!")
+newspaper("adlfjljksanfdlsa")
+
+print("C:\\Windows")
+
+list_ = {"naver", "kakao", "sk", "samsung"}
+
+print("naver", "kakao", "sk", "samsung", sep=';')
