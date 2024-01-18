d0770595d42a03a98334c4035296a61b011ba96f
Jijhoon <darkmine789@naver.com>
2024-01-18 15:46:52 +0900

    yield & decoreators

--- helllo.py
+++ helllo.py
@@ -1,51 +1,61 @@
-# prime number with function
-def isprime(n) -> bool:
+#lambda : 영구적인 함수 // 자주쓰는 함수라서 미리 만들어 놓는다.
+def squares(n):
+    return n * n
+even_numbers = [i for i in range(51) if i % 2 == 0]
+print(even_numbers)
+print(tuple(map(squares, even_numbers)))
+print(tuple(map(lambda x: x**2, even_numbers)))
+
+# 구조 >>> lambda input: input이 실행되는 식.
+
+# generator는 메모리에 저장하지 않는다. 숫자 생성하고 다음숫자 생성할때 이전 숫자를 지워버린다. ex) range()
+
+def my_range(first = 0, last = 5, step = 1):
+    number = first
+    while number < last:
+        yield number #함수의 실행 상태를 유지하면서 값을 순차적으로 반환 // 큰 데이터를 처리할 때 메모리를 효율적으로 사용
+        number += step
+r = my_range()
+print(r,type(r))
+
+for x in r : # 0~4까지 출력됨.
+    print(x)
+for x in r: #한번 사용한 값들이기 때문에 휘발해버렸음.
+    print(x)
+
+#Decorators  OCP 개방폐쇄원칙(open closed principle) : 확장에는 열려있고 수정에는 닫혀야한다.
+#document_it() // 함수 출력과 인수 출력을 해준다.
+
+# decorator
+def description(f):  # closure
+    def inner(*args):
+        print(f.__name__)
+        print(f.__doc__)
+        r = f(*args)
+        return r
+
+    return inner
+
+
+def squares(n):
     """
-    매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
-    :param n: 판정할 매개변수
-    :return: 소수면 True, 소수가 아니면 False
+    제곱 함수
     """
-    if n < 2:
-        return False
-    else:
-        i = 2
-        while i*i <= n:
-            if n % i == 0:
-                return False
-            i += 1
-        return True
+    return n * n
+
+@description # 얘가 붙음으로써 데코레이팅이 된다.
+def power(b, e):
+    """
+    거듭제곱 함수
+    """
+    result = 1
+    for _ in range(e):
+        result = result * b
+    return result
 
 
-while True:
-    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
-
-    if menu == '1':
-        fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
-    elif menu == '2':
-        celsius = float(input('Input Celsius : '))
-        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
-    elif menu == '3':
-        number = int(input("Input number : "))
-        if isprime(number):
-            print(f'{number} is prime number')
-        else:
-            print(f'{number} is NOT prime number!')
-    elif menu == '4':
-        n1, n2 = map(int, input("Input first second number : ").split())
-        # numbers = input("Input first second number : ").split()
-        # n1 = int(numbers[0])
-        # n2 = int(numbers[1])
-        n1, n2 = min(n1, n2), max(n1, n2)
-        # if n1 > n2:
-        #     n1, n2 = n2, n1
-
-        for number in range(n1, n2 + 1):
-            if isprime(number):
-                print(number, end=' ')
-        print()
-    elif menu == '5':
-        print('Terminate Program.')
-        break
-    else:
-        print('Invalid Menu!')
+f1 = description(squares)
+print(f1(9))
+print(power(2, 10))
+# f2 = description(power)
+# print(f2(2, 10))
