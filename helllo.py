bcecb3ca7542bc7fe8d7decb62c4482a61ca3778
Jijhoon <darkmine789@naver.com>
2024-01-18 10:55:20 +0900

    prime number with function

--- helllo.py
+++ helllo.py
@@ -1,25 +1,31 @@
-empty_set = set() # 비어있는 set을 만드는 유일한 방법.
-# print(set({'alphabet': 'A', 'country': 'Korea', 'fruits': 'Apple' })) # 순서개념이 존재하지 않는다. 그래서 랜덤의 순서로 나온다.
-# print(set(("het", "hey", "what"))) # 순서개념이 존재하지 않는다. 그래서 랜덤의 순서로 나온다.
-# print(set(("D", "A", "C", "B", "R", "T"))) # 순서개념이 존재하지 않는다. 그래서 랜덤의 순서로 나온다.
-# print(set(dict[aaa:'c', b:"r", qq: "re"]))
+#Functinon : 코드를 재사용 한다.
+#SOLID의 원칙.
+#S RP : 단독챔임의 원칙(함수 하나가 하나의 기능을 제대로 작동해야한다.) //self responsibillity
+# prime number
+def isprime(n) -> bool:
+    """
+    매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
+    :param n: 판정할 매개변수
+    :return: 소수면 True, 소수가 아니면 False
+    """
+    if n < 2:
+        return False
+    else:
+        i = 2
+        while i*i <= n:
+            if n % i == 0:
+                return False
+            i += 1
+        return True
 
-a = {1, 2, 5}
-b = {2, 3, 4}
-print(a & b) # 교집합
-print(a.intersection(a, b)) # 교집합
 
-print(a.union(b)) # 합집합
+numbers = input("Input first second number : ").split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
 
-print(a.difference(b)) # a - b 차집합
+if n1 > n2:
+    n1, n2 = n2, n1
 
-print(a^b) # 합집합 - 교집합 : 다르게 가지고 있는 값들.
-print(a.symmetric_difference(b)) # # 합집합 - 교집합
-
-freeze = frozenset([1, 5, 4, 32, 62, 34, 17, 134])
-print(freeze) # frozenset({32, 1, 34, 4, 5, 134, 17, 62})
-freeze = frozenset[1, 5, 4, 32, 62, 34, 17, 134]
-print(freeze) # frozenset[1, 5, 4, 32, 62, 34, 17, 134]
-
-freeze = frozenset(1, 5, 4, 32, 62, 34, 17, 134) # 이건 tuple로 인식되었다. 그래서 freeze는 1개인데 8개의 숫자가 와서 오류가 났다.
-print(freeze)
+for number in range(n1, n2+1):
+    if isprime(number):
+        print(number, end=' ')
