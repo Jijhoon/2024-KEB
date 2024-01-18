c7af0c05d4dd2429e4789fd6bb04c6b565bedbce
Jijhoon <darkmine789@naver.com>
2024-01-18 10:22:40 +0900

    about set

--- helllo.py
+++ helllo.py
@@ -1,15 +1,25 @@
-univ ="inha university"
-counts_alphabet = {letter: univ.count(letter) for letter in univ}
-print(counts_alphabet) # {'i': 3, 'n': 2, 'h': 1, 'a': 1, ' ': 1, 'u': 1, 'v': 1, 'e': 1, 'r': 1, 's': 1, 't': 1, 'y': 1}
+empty_set = set() # 비어있는 set을 만드는 유일한 방법.
+# print(set({'alphabet': 'A', 'country': 'Korea', 'fruits': 'Apple' })) # 순서개념이 존재하지 않는다. 그래서 랜덤의 순서로 나온다.
+# print(set(("het", "hey", "what"))) # 순서개념이 존재하지 않는다. 그래서 랜덤의 순서로 나온다.
+# print(set(("D", "A", "C", "B", "R", "T"))) # 순서개념이 존재하지 않는다. 그래서 랜덤의 순서로 나온다.
+# print(set(dict[aaa:'c', b:"r", qq: "re"]))
 
-#같은 결과 다른 방법 위에꺼는 dictionary의 축약형이 가능한 것을 보여준다.
-counts_alphabet = dict()
-for letter in univ:
-    counts_alphabet[letter] = univ.count(letter)
-print(counts_alphabet) # {'i': 3, 'n': 2, 'h': 1, 'a': 1, ' ': 1, 'u': 1, 'v': 1, 'e': 1, 'r': 1, 's': 1, 't': 1, 'y': 1}
+a = {1, 2, 5}
+b = {2, 3, 4}
+print(a & b) # 교집합
+print(a.intersection(a, b)) # 교집합
 
-# Assignment ex) 8.10
-squares = {n: pow(n, 2) for n in range(10)} # {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81}
-#squares = {n: n**2 for n in range(10)} # {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81}
-#squares = {n: n*n for n in range(10)} # {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81}
-print(squares)
+print(a.union(b)) # 합집합
+
+print(a.difference(b)) # a - b 차집합
+
+print(a^b) # 합집합 - 교집합 : 다르게 가지고 있는 값들.
+print(a.symmetric_difference(b)) # # 합집합 - 교집합
+
+freeze = frozenset([1, 5, 4, 32, 62, 34, 17, 134])
+print(freeze) # frozenset({32, 1, 34, 4, 5, 134, 17, 62})
+freeze = frozenset[1, 5, 4, 32, 62, 34, 17, 134]
+print(freeze) # frozenset[1, 5, 4, 32, 62, 34, 17, 134]
+
+freeze = frozenset(1, 5, 4, 32, 62, 34, 17, 134) # 이건 tuple로 인식되었다. 그래서 freeze는 1개인데 8개의 숫자가 와서 오류가 났다.
+print(freeze)
