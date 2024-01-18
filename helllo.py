cc02df5b8c9cb0c699458c90c332fb104bdcc72b
Jijhoon <darkmine789@naver.com>
2024-01-18 14:19:00 +0900

    closures

--- helllo.py
+++ helllo.py
@@ -1,43 +1,43 @@
-#일반적으로는 keyword argument를 이용하지만
-#positional argument
-# argument : 인수 / 매개변수 : Parameters
-# * : 가변 매개변수 // argue가 많을 때 그 만큼의 방을 제공해준다. 인수가 없으면 비어있는 튜플 등을 출력한다.
-# *arg : optonal positional argument : 각 방에 맞게 순서대로 배치하기 때문에 positional
-# **kwarg : optional keyword argument : 순서 상관없이 key에 맞게 value가 배치되기 때문에 keyword.
+def outer(a, b):
+    def inner(c, d):
+        return c + d
+    return inner(a, b)
+print(outer(4, 6)) # 10
 
-def a(n):
-    if n is None:
-        print(f"{n}: There is not space")
-    elif a is True:
-        print((f"{n}: There is space"))#???
-    else:
-        print((f"{n}: There is not itme in space"))
+def out(word):
+    def inn(quote):
+        return "HiHi It's a nice day, '%s' "%quote
+    return inn(word)
 
-a("")
-a(" ")
-a([])
-a([0])
-a(0)
-a(None)
-a("true")
-a(True)
+print(out(('MEMEME'))) #HiHi It's a nice day, 'MEMEME'
 
+# def out_func(nout):
+#     def inner_func(nin):
+#         return nin * nin
+#     return inner_func(nout)
+# print(out_func(5))
+#closures 안에 들어있는 함수의 주소를 가져옴. 변수 하나를 정해서 내부의 function으로 만들 수 있음.
+def out_func(nout):
+    def inner_func():
+        return nout * nout
+    return inner_func
+x = out_func(9) #closures에서 중요한 것은 외부함수에서의 argument를 내부함수가 copy해서 작동한다는 것이다.
+print(type(x)) #<class 'function'>
 
-def squares(*n):
-    """
-    넘겨 받은 수치 데이터들의 거듭제곱 값을 리스트에 담아서 리턴
-    :param n: tuple
-    :return: list
-    """
-    return [pow(i, 2) for i in n ]
+print()
 
-print(squares(5))
+print(x) #<function out_func.<locals>.inner_func at 0x0000025B348EF060>
 
-def run_function(f, number):
-    return squares(number)
-print(squares(8))
-print(run_function(squares,9))
+print()
 
-def new_run(f,*args) -> list:
-    return f(*args)
-print(new_run(squares,5, 6, 7))
+print(x()) #81 -> x는 함수이기 때문에 소괄호를 포함해야 작동한다.
+
+numbes = ["-7", "11", "3"]
+print(sum(map(int, numbes)))
+
+#같은 결과 더 긴 방법
+numbes = ["-7", "11", "3"]
+hap = 0
+for number in numbes:
+    hap = hap + int(number)
+print(hap)
