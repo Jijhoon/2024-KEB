932a7b8bf02f8b67244090c6a80cd90fcfbeda95
Jijhoon <darkmine789@naver.com>
2024-01-19 15:05:31 +0900

    horse

--- helllo.py
+++ helllo.py
@@ -1,36 +1,18 @@
-class Pokemon:
-    def __init__(self, name):
-        self.name = name
-
-    def attack(self, target):
-        print(f"{self.name}이(가) {target.name}을(를) 공격!")
-
-#class Pikachu:
-class Pikachu(Pokemon):  # is-a
-    def __init__(self, name, type):
-        super().__init__(name)
-        self.type = type
-
-    def attack(self, target):
-        print(f"{self.name}이(가) {target.name}을(를) {self.type} 공격!")
-
-    def electric_info(self):
-        print("전기 계열의 공격을 합니다")
-
-
-class Squirtle(Pokemon):  # is-a
+class Animal:
+    def says(self):
+        return '아이 엠 동물'
+class Horse(Animal):
     pass
 
-class Agumon:
+class Donkey(Animal):
     pass
-
-p1 = Pikachu("피카츄", "전기")
-p2 = Squirtle("꼬부기")
-p3 = Pokemon("아무개")
-p1.electric_info()
-#p3.electric_info()  # AttributeError: 'Pokemon' object has no attribute 'electric_info'
-p1.attack(p2)
-p2.attack(p1)
-print(p1.name, p1.type)
-print(issubclass(Pikachu, Pokemon))
-print(issubclass(Agumon, Pokemon))
+class Mule(Donkey, Horse):
+    pass
+class Hinny(Horse, Donkey):
+    # def says(self):
+    #     return '버새 버새' # 없으면 계속 거슬러 올라간다.
+    pass
+m1 = Mule()
+h1 = Hinny()
+print(Hinny.__mro__)
+print(h1.says())
