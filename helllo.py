bbef45b4211795375b63ca0bb0e0100716b0ff51
Jijhoon <darkmine789@naver.com>
2024-01-19 10:35:54 +0900

    recursion function

--- helllo.py
+++ helllo.py
@@ -1,96 +1,39 @@
-#9.1 Define a function called good() that returns the following list: ['Harry', 'Ron', 'Hermione'].
-# def good():
-#     return ['Harry', 'Ron', 'Hermione']
-# print(good())
-def good():
-    harry_poter = input().split()
-    return harry_poter
-print(good())
-# input >>> hi my name is poter
-# ['hi', 'my', 'name', 'is', 'poter']
+year = "2020"
+for i in range(1,4):
+    print(int(year) - i)
 
-# 9.2 Define a generator function called get_odds() that returns the odd numbers from range(10).
-# Use a for loop to find and print the third value returned.
-# def get_odds():
-# #     for i in range(1, 10):
-# #         if i % 2 == 1:
-# #             yield i
-# #
-# #
-# # for number in get_odds():
-# #     print(number)
-def get_odds(n):
-    for i in range(1, n+1, 2):
-            yield i
-cnt = 0
-odds = get_odds(10)
-for odd in odds:
-    cnt += 1
-    if cnt == 3:
-        print(f'Third number is {odd}')
-        break
+letters = 'python'
+print(letters[0:3:2] + "\t")
 
-for number in get_odds(10):
-    print(number)
+#Recursion 재귀함수.
+def dive():
+    return dive()
 
-#9.3 Define a decorator called test that prints 'start' when a function is called, and 'end' when it finishes.
+def factorial_repetition(n) -> int: # 팩토리얼 (결과 * i) 반복
+    result = 1
+    for i in range(1, n+1):
+        result = result * i
+    return result
+print(factorial_repetition(int(input("number : "))))
 
-def test(f):
+def factorial_recursion(n):
     """
-    데코레이터 함수, 함수 시작하면 start 출력, 함수 끝나면 end 출력
-    :param f: function
-    :return: closure function
+    재귀함수를 사용한 팩토리얼 함수
+    :param n: 정수, int
+    :return: function, int
     """
-    def wrapper(*args, **kwargs):
-        print("start")
-        result = f(*args, **kwargs)
-        print("end")
-        return result
-    return wrapper
+    if n == 1:
+        return n
+    else:
+        return n * factorial_recursion(n-1) # n * (n-1)!
+number = int(input("nuber : "))
+print(factorial_recursion(number))
+print(factorial_repetition(number))
+print(globals())
 
-# @test
-# def func():
-#     return print("hi")
-# func()
+# 반복문은 속도가 매우 빠름. 재귀함수는 속도가 느리지만 가독성이 높고 실수할 확률이 적음.
 
-def func():
-    return print("hi")
-t = test(func)
-t()
+#Async Function (ASYNCHRONOUS : 비동기화) // 다 다르게 움직이는 함수. -> 각자 할 일을 하니까 일처리 속도가 빨라짐.
+# -> 동기화를 하면 속도가 느려짐. but
 
-def pupupu(arg,result = []):
-    result.append(arg)
-    return print(result)
 
-pupupu("a")
-pupupu("b")
-
-def popopo(arg, result = None):
-    if result is None:
-        result = []
-        result.append(arg)
-    return print(result)
-
-popopo("a")
-popopo("b")
-
-def ricecake(saysomething):
-        def cake():
-            return "I am delicious'%s'" % saysomething
-        return cake
-
-a = ricecake("hi") #이런 상태를 만들어야만 내부의 함수를 볼 수 있다. -> I am delicious'hi'
-print(a())
-print(ricecake("hi")) # >>> <function ricecake.<locals>.cake at 0x0000021B1EE0F420>
-
-def newspaper(word):
-    return print(word.capitalize() + "!")
-newspaper("adlfjljksanfdlsa")
-
-print("C:\\Windows")
-
-list_ = {"naver", "kakao", "sk", "samsung"}
-
-print("naver", "kakao", "sk", "samsung", sep=';')
-
-print(list_)
