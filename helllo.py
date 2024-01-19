e1e92b27d2a9c79d630f3dc808ab12eba519bf1f
Jijhoon <darkmine789@naver.com>
2024-01-19 13:50:59 +0900

    about class

--- helllo.py
+++ helllo.py
@@ -1,14 +1,24 @@
-def desc(a):
-    def wrapper(): # 내부의 함수는 별도의 공간을 가지고 있어서 외부함수를 그냥 실행시켜도 return되지 않음.
-        print("booboobooboo")
-        a()
-    # print("a")
-    return wrapper
- #desc()   #실행시키면 a만 나옴.
-@desc
-def something():
-    print("do something")
+class Pokemon:
+    pass
+    def __init__(self, name):
+        self.name = name  #pikachu.name = "피카츄" , squirtle.name = "꼬부기" // 객체를 생성할 때 호출되는 영역.
+        print(f"production of poketmonster of {name}") # pikachu = Pokemon("피카츄") 이렇게 넣어준다.
+    def attack(self, target):
+        print(f"{self.name}이(가) {target.name}을(를) 공격!")
+pikachu = Pokemon("피카츄") # init은 각 개체마다 딱 한 번 이름을 지어주는 것이다.
+squirtle = Pokemon("꼬부기")
+charizard = Pokemon("리자몽")
 
-something()
-# s = desc(something) // 변수지정을 함으로써 내부의 함수를 작동시킬 수 있다. 내부함수는 외부함수에대한 정보를 받아들이는 것이다.
-# s()
+pikachu.nemesis = squirtle
+print(pikachu.name)
+print(pikachu.nemesis.name)
+print(squirtle.name)
+
+# print()
+# charizard.attack(squirtle)
+#  # is a : 상속 관계 (Inherent) // has a : 연관 관계 // use a : 사용관계
+class Pikachu(Pokemon): # is -a 피카츄는 포켓몬이다.
+    pass
+p1 = Pikachu("피카츄")
+print(p1.name)
+
