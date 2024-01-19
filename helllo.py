5d16fb44e62f5f14b32e20b76b4e9e66ba1da0e5
Jijhoon <darkmine789@naver.com>
2024-01-19 15:25:28 +0900

    naming

--- helllo.py
+++ helllo.py
@@ -11,7 +11,14 @@ class SwmmingMixin:
 class Pokemon:
     def __init__(self, name):
         self.name = name
-
+    def attack(self):
+        print("공격!")
+    def get_name(self):
+        print("inside getter")
+        return self.name
+    def set_name(self, new_name):
+        print("inside setter!!!")
+        self.name = new_name
 class Charizard(Pokemon, FlyingMixin):
     pass
 class Gyarados(Pokemon, SwmmingMixin):
@@ -20,3 +27,9 @@ g1 = Gyarados("갸라도스")
 c1 = Charizard("리자몽")
 print(c1.fly())
 print(g1.swim())
+print(g1.name)
+g1.set_name("잉어킹")
+print(g1.name)
+c1.attack()
+Charizard.attack(c1)
+#Charizard.attack() # Charizard는 class 명이기 때문에 오류가 발생 객체명인 c1이 와야 실행이 된다.
