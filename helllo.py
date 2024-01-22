9874821936b70d3d0223debce9d2d789f8a41a08
Jijhoon <darkmine789@naver.com>
2024-01-22 19:03:41 +0900

    pokemon 3

--- helllo.py
+++ helllo.py
@@ -1,36 +1,154 @@
-# module
-#from mymath import *
-import mymath as mm
+import random
 
-while True:
-    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
+class Pokemon:
+    def __init__(self, name):
+        self.name = name
+    def attack(self, enemy):
+        print(f"{self.name} attacked {enemy}!")
 
-    if menu == '1':
-        fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}F, Celsius : {mm.fahrenheit_to_celsius(fahrenheit):.4f}C')
-    elif menu == '2':
-        celsius = float(input('Input Celsius : '))
-        print(f'Celsius : {celsius}C, Fahrenheit : {mm.celsius_to_fahrenheit(celsius):.4f}F')
-    elif menu == '3':
-        number = int(input("Input number : "))
-        if mm.isprime(number):
-            print(f'{number} is prime number')
-        else:
-            print(f'{number} is NOT prime number!')
-    elif menu == '4':
-        numbers = input("Input first second number : ").split()
-        n1 = int(numbers[0])
-        n2 = int(numbers[1])
+class Flame:
+    def flamethrower(self,enemy):
+        print(f"{self.name} used the skill 'Flamethrower' on {enemy}")
+class Electric:
+    def thunderbolt(self, enemy):
+        print(f"{self.name} used the skill 'Thunderbolt' on {enemy}")
+class Water:
+    def water_Pulse(self, enemy):
+        print(f"{self.name} used the skill 'Water Pulse' on {enemy}")
+class Normal:
+    def arm_Thrust(self, enemy):
+        print(f"{self.name} used the skill 'Arm Thrust' on {enemy}")
+class Dragon:
+    def hyper_beam(self, enemy):
+        print(f"{self.name} used the skill 'Hyper Beam' on {enemy}")
 
-        if n1 > n2:
-            n1, n2 = n2, n1
+class Charmander(Pokemon, Flame):
+    def __init__(self, name, _type):
+        super().__init__(name)
+        self._type = _type
+class Pikachu(Pokemon, Electric):
+    def __init__(self, name, _type):
+        super().__init__(name)
+        self._type = _type
+class Squitle(Pokemon, Water):
+    def __init__(self, name, _type):
+        super().__init__(name)
+        self._type = _type
+class Dialga(Pokemon, Dragon):
+    def __init__(self, name, _type):
+        super().__init__(name)
+        self._type = _type
 
-        for number in range(n1, n2 + 1):
-            if mm.isprime(number):
-                print(number, end=' ')
-        print()
-    elif menu == '5':
-        print('Terminate Program.')
-        break
-    else:
-        print('Invalid Menu!')
+class Mareep(Pokemon, Electric):
+    def __init__(self, name, _type):
+        super().__init__(name)
+        self._type = _type
+class Octillery(Pokemon, Water):
+    def __init__(self, name, _type):
+        super().__init__(name)
+        self._type = _type
+class Houndoom(Pokemon, Flame):
+    def __init__(self, name, _type):
+        super().__init__(name)
+        self._type = _type
+class Snorlax(Pokemon, Normal):
+    def __init__(self, name, _type):
+        super().__init__(name)
+        self._type = _type
+class Rayquaza(Pokemon, Dragon):
+    def __init__(self, name, _type):
+        super().__init__(name)
+        self._type = _type
+        super().__init__(_type)
+
+p1 = Charmander("Charmander", "(Flame)" )
+p2 = Pikachu("Pikachu", "(Electric)" )
+p3 = Squitle("Squitle", "(Water)" )
+p4 = Dialga("Dialga", "(Dragon)" )
+
+e1 = Mareep("Mareep", "(Electric)" )
+e2 = Octillery("Octillery", "(Water)" )
+e3 = Houndoom("Houndoom", "(Flame)" )
+e4 = Snorlax("Snorlax", "(Normal)" )
+e5 = Rayquaza("Rayquaza", "(Dragon)" )
+
+enemy = random.randint(1,5)
+if enemy == 1:
+    enemy = e1.name + e1._type
+elif enemy == 2:
+    enemy = e2.name + e2._type
+elif enemy == 3:
+    enemy = e3.name + e3._type
+elif enemy == 4:
+    enemy = e4.name + e4._type
+elif enemy == 5:
+    enemy = e5.name+ e5._type
+
+else:
+    print("Rewrite the number.")
+# print(p1.name + "(" + p1._type + ")")
+# Flame.flamethrower(e3, p1)
+# Pokemon.attack(p1, e2)
+import os
+
+if input("Press Enter to Continue \n"):
+    next
+
+print("Nice to meet you\n Could you tell me what your name is?")
+master = input()
+
+print(f"{master}... It's cool.\n")
+print("I will tell you some information.\t"
+      "You have 4 Pokemons\n"
+      f"1) {p1.name + p1._type}\t"
+      f"2) {p2.name + p2._type}\t"
+      f"3) {p3.name + p3._type}\t"
+      f"4) {p4.name + p4._type}\t"
+      )
+print("\n\nNow, let's go on a adventure")
+selection = input("1)To the Forest 2)To the Jungle 3)To the Desert\n Where do you want to go? : ")
+if int(selection) == 1:
+    print(f"A wild {enemy} appeared.")
+elif int(selection) == 2:
+    print(f"A wild {enemy} appeared.")
+elif int(selection) == 3:
+    print(f"A wild {enemy} appeared.")
+
+meeting = int(input("What will you do? \n1)Fight 2)Runway \n :"))
+if meeting == 1:
+    battle = int(input(
+        f"1) {p1.name + p1._type}\t"
+        f"2) {p2.name + p2._type}\t"
+        f"3) {p3.name + p3._type}\t"
+        f"4) {p4.name + p4._type}\t"
+        "Choose your pokemon! : "
+        ))
+    while battle == 1:
+        battle1 = int(input("1)Attack 2)skill \n What is your next behavior? : "))
+        if battle1 == 1:
+            Pokemon.attack(p1, enemy)
+        elif battle1 == 2:
+            Flame.flamethrower(p1, enemy)
+    while battle == 2:
+        battle1 = int(input("1)Attack 2)skill \n What is your next behavior? : "))
+        if battle1 == 1:
+            Pokemon.attack(p2, enemy)
+        elif battle1 == 2:
+            Electric.thunderbolt(p2, enemy)
+    while battle == 3:
+        battle1 = int(input("1)Attack 2)skill \n What is your next behavior? : "))
+        if battle1 == 1:
+            Pokemon.attack(p3, enemy)
+        elif battle1 == 2:
+            Water.water_Pulse(p3, enemy)
+    while battle == 4:
+        battle1 = int(input("1)Attack 2)skill \n What is your next behavior? : "))
+        if battle1 == 1:
+            Pokemon.attack(p4, enemy)
+        elif battle1 == 2:
+            Dragon.hyper_beam(p4, enemy)
+# 지금 enemy로 dragon이 나오면 (dragon)(dragon)으로 표기되고 있다 오류를 찾아보자.
+
+
+
+
--- pythob/pupupu.py
+++ pythob/pupupu.py
@@ -1,26 +1,2 @@
-#Number Guessing Game
 import random
-user_name = input("Write your name : ")
-number = random.randint(1,100)
-print("We will play the game.\nGuess the number,1 to 100")
-try_op = 0
-while try_op < 7:
-    try_op += 1
-    try:
-        i = int(input("number what you expected : "))
-
-        if isinstance(i,int):
-            if number < i:
-                print("\nIt's too high!")
-            elif number > i:
-                print("\nIt's too low!")
-            elif number == i:
-                break
-        else:
-            print("system error")
-    except ValueError:
-        print("\nThat's wrong way! You lost one chance!")
-if number == i:
-    print(f"\ncongratulation! {user_name}, you win!")
-else:
-    print(f"\nYou lose, {user_name}. The answer was {number}. Try next time!")
+help(random)
