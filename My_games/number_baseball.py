7f2c6e9f1ce72ee42e95f50d5f0efdd2d4229597
Jijhoon <darkmine789@naver.com>
2024-01-26 19:01:34 +0900

    baseball gaem nv.1

--- My_games/number_baseball.py
+++ My_games/number_baseball.py
@@ -1,82 +1,79 @@
 import random
-# Number Baseball Game (NBG)
 
+class Digits_checker:
+    def digit_check(user_try): #TODO: 유저가 입력한 숫자와 랜덤숫자의 자릿수가 다르다면 자릿수가 다르다고 출력. 같으면 strike ball out 출력.
+        # if user_try
         
-class Checker():
-    def __init__(self):
-        self.check_strike()
-        self.check_ball()
-        self.out()
+        
 
-    def check_strike(t_n, digi):
-        cnt_s = 0
-        for i in range(0, digi):
-            if list(range(digi))[i] == t_n[i]:
-                cnt += 1
-        print(cnt_s)
+if __name__ == "__main__":
+    while True: 
+        start = input("Do you want to play number baseball game? (Y / N)\n:").casefold()
+        if start.strip() == "y":
+            break
+        elif start.strip() == "n":
+            exit()
+        else:
+            print("Please write the Y or N")
+            continue
 
-    def check_ball(t_n, digi):
-        cnt_b = 0
-        for i in range(0, digi):
-            for y in range(0, digi):
-                if (list(range(digi))[i] == t_n[y]) and (list(range(digi)) != t_n):
-                    cnt_b += 1
-        print(cnt_b)
+    user_name = input("Please input your nickname: ")
+      
+    while True:
+        try:
+            user_input_digits = int(input("How many digits of game do you want?: "))
+            if user_input_digits > 0:
+                break
+            else:
+                print("Please write a integer of plus")    
+        except:
+            print("Please write a integer of plus")
 
-    def out(t_n, b_n):
-        if (b_n[i] != t_n[y]) and (b_n != t_n):
-            print("You are out")
-
-class Difference():
-    def differ(self):
-        self.__init__() 
-        if (cnt_s >= 1) or (cnt_b >= 1):
-            print(f"You have {check_strike()}-stirke and {check_ball()}-ball")
-        else:    
-            print(f"{out()}")
-
-identifying = Checker()
-
-
-
+    game_digits = [random.randint(0, 9) for i in range(user_input_digits)]
+    game_digits[0] = random.randint(1, 9) # NOTE: 게임에서 정한 랜덤 숫자리스트 = game_digits
+    sequence = 1
+    while True:
+        for i in range(0,3):
+            if i == 1:
+                ordinal_number = "st"
+                user_try = list(input(f"Your {sequence}{ordinal_number} attempt: "))
+                for i in range(0, user_input_digits):
+                    user_try[i] = int(user_try[i])
+                sequence += 1
+                
+            elif i == 2:
+                ordinal_number = "nd"
+                user_try = list(input(f"Your {sequence}{ordinal_number} attempt: "))
+                for i in range(0, user_input_digits):
+                    user_try[i] = int(user_try[i])
+                sequence += 1
+            elif i == 3:
+                ordinal_number = "rd"
+                user_try = list(input(f"Your {sequence}{ordinal_number} attempt: "))
+                for i in range(0, user_input_digits):
+                    user_try[i] = int(user_try[i])
+                sequence += 1
+            else:
+                user_try = list(input(f"Your {sequence}{ordinal_number} attempt: "))
+                for i in range(0, user_input_digits):
+                    user_try[i] = int(user_try[i])
+                sequence += 1
+     
     
 
-while True:
-    start = input("Do you want play the Game?(y / n)\n: ").casefold()
-
-    if start.strip() == "y":
-        break
-    elif start.strip() == "n":
-        print("Good bye")
-        exit()
-    else:
-        print("Answer Y or N")
-
-user_name = input("Write your nickname: ")
-
-digits = int(input("How many digits of the game do you want?: ")) 
-baseball_number = list(range(digits))
-baseball_number[0] = random.randint(1, 9)
-for i in range(1, len(baseball_number), ):
-    baseball_number[i] = random.randint(0, 9)
+        
+    
+    
 
 
-while True: #TODO:점수가 0점이될때 false:
-    try_number = list(input())
-    attempt = 1
-    try_number = list(input(f"The {attempt}st try : "))
-    identifying()
 
-    attempt += 1
-    try_number = list(input(f"The {attempt}nd try : "))
-    identifying()
-    attempt += 1
-    try_number = list(input(f"The {attempt}rd try : "))
-    identifying()
-    attempt += 1
-    while True:
-        try_number = list(input(f"The {attempt}th try : "))
-        identifying()
-        attempt += 1
+
+
+
+        
+
+
+
+
 
 
--- My_games/test.py
+++ My_games/test.py
@@ -1,6 +1,12 @@
 import random
 # test = [1, 2, 3]
 # len(3) -> 3자리 리스트
-
-number = list(input())
-print(number)
+# number = list(input()) -> 문자열로된 리스트. 
+# number = list(input())
+# for i in range(0,len(number)):
+#     number[i] = int(number[i])
+# print(number)
+a = list(input())
+for i in range(0, 4):
+    a[i] = int(a[i])
+print(a)
